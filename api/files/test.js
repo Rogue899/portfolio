443c161bae88f5239eca18d9cf374f485fc841fb
Tarek Roukos <t.arek.r94@gmail.com>
2025-11-13 17:35:47 +0200

    Add simple test endpoint to debug routing

---
+++ api/files/test.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  return res.status(200).json({
+    success: true,
+    message: 'Test endpoint works!',
+    method: req.method,
+    query: req.query
+  });
+}
+
